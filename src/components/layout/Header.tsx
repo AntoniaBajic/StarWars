5de73d1f558153a146ab672f7987f16dc40c6a1c
Antonia Bajic <antoniaa.bajic@gmail.com>
2020-11-05 22:02:34 +0100

    project setup

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className='header'>
+      <span className='SWStarter'>SWStarter</span>
+    </header>
+  );
+};
+
+export default Header;
